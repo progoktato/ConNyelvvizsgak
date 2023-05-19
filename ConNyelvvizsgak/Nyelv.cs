0c8ddb9be48b63208268f49037f89cfbc9fe1937
Progoktato <oktato.ber@gmail.com>
2023-05-19 18:54:51 +0200

    Add project files.

---
+++ ConNyelvvizsgak/Nyelv.cs
@@ -0,0 +1,103 @@
+﻿namespace ConNyelvvizsgak
+{
+    public class Nyelv
+    {
+        const int BAZIS_EV = 2009;
+        string nev;
+        List<int> sikeresVizsgak;
+        List<int> sikertelenVizsgak;
+
+        public Nyelv(string nyelv, List<int> sikeresVizsgak, List<int> sikertelenVizsgak)
+        {
+            this.nev = nyelv;
+            this.sikeresVizsgak = sikeresVizsgak;
+            this.sikertelenVizsgak = sikertelenVizsgak;
+        }
+
+        public string Nev { get => nev; }
+        public List<int> SikeresVizsgak { get => sikeresVizsgak; }
+        public List<int> SikertelenVizsgak { get => sikertelenVizsgak; }
+
+        //Teljes időszakra
+        public int EddigiOsszesViszgakSzama { get => sikeresVizsgak.Sum() + sikertelenVizsgak.Sum(); }
+        public double SikeressegiAranyTeljesIdoszakban { get => 100d * sikeresVizsgak.Sum() / EddigiOsszesViszgakSzama; }
+        public double SikertelensegiAranyTeljesIdoszakban { get => 100 - SikeressegiAranyTeljesIdoszakban; }
+
+        //Csak egy évre
+        public int VizsgakSzamaAdottEvben(int evIndex)
+        {
+            return sikeresVizsgak[evIndex - BAZIS_EV] + sikertelenVizsgak[evIndex - BAZIS_EV];
+        }
+        public double SikeressegiAranyAdottEvben(int evIndex)
+        {
+            int osszesVizsga = VizsgakSzamaAdottEvben(evIndex);
+            return osszesVizsga == 0 ? -1 : sikeresVizsgak[evIndex - BAZIS_EV] * 100d / osszesVizsga;
+        }
+        public double SikertelensegiAranyAdottEvben(int evIndex)
+        {
+            return VizsgakSzamaAdottEvben(evIndex) != 0 ? 100 - SikeressegiAranyAdottEvben(evIndex) : -1;
+        }
+    }
+
+    //Miben más?
+    //A konstruktorban több mindent előre kiszámolunk. Igaz több a tárigény, de gyorsabb a futtatás!
+
+    /*
+public class Nyelv
+{
+    const int BAZIS_EV = 2009;
+    string nev;
+    List<int> sikeresVizsgak;
+    List<int> sikertelenVizsgak;
+
+    //Ha a CPU terhelést akarom csökkenteni, akkor:
+    List<int> osszesVizsga;
+    int osszesVizsgaSzama;
+    int osszesSikeresVizsgaSzama;
+
+
+    public Nyelv(string nyelv, List<int> sikeresVizsgak, List<int> sikertelenVizsgak)
+    {
+        this.nev = nyelv;
+        this.sikeresVizsgak = sikeresVizsgak;
+        this.sikertelenVizsgak = sikertelenVizsgak;
+
+        //CPU miatt, dönthetünk úgy, hogy egyszer számoljuk ki és inkább tároljuk
+        //Ez akkor jó csak, ha az adatok nem változnak!
+        osszesVizsga = sikertelenVizsgak.Zip(sikeresVizsgak, (elsobol, masodikbol) => elsobol + masodikbol).ToList();
+        this.osszesSikeresVizsgaSzama = sikeresVizsgak.Sum();
+        this.osszesVizsgaSzama = osszesSikeresVizsgaSzama + sikertelenVizsgak.Sum();
+
+    }
+
+    public string Nev { get => nev; }
+    public List<int> SikeresVizsgak { get => sikeresVizsgak; }
+    public List<int> SikertelenVizsgak { get => sikertelenVizsgak; }
+
+    //Teljes időszakra
+    public int EddigiOsszesViszgakSzama { get => this.osszesVizsgaSzama; }
+
+    public double SikeressegiAranyTeljesIdoszakban { get => 100d * osszesSikeresVizsgaSzama / this.osszesVizsgaSzama; }
+
+    public double SikertelensegiAranyTeljesIdoszakban { get => 100 - SikeressegiAranyTeljesIdoszakban; }
+
+    //Csak egy évre
+    public int VizsgakSzamaAdottEvben(int evIndex)
+    {
+        return osszesVizsga[evIndex - BAZIS_EV];
+    }
+
+    public double SikeressegiAranyAdottEvben(int evIndex)
+    {
+        int osszesVizsga = this.osszesVizsga[evIndex - BAZIS_EV];
+        return osszesVizsga == 0 ? -1 : sikeresVizsgak[evIndex - BAZIS_EV] * 100d / osszesVizsga;
+    }
+
+    public double SikertelensegiAranyAdottEvben(int evIndex)
+    {
+        return this.osszesVizsga[evIndex - BAZIS_EV] != 0 ? 100 - SikeressegiAranyAdottEvben(evIndex) : -1;
+    }
+}
+*/
+
+}
---
+++ ConNyelvvizsgak/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace ConNyelvvizsgak
+{
+    internal class Program
+    {
+        static List<Nyelv> nyelvek;
+
+        //1.
+        private static List<Nyelv> AdatokBetoltese(string sikeres, string sikertelen)
+        {
+            List<Nyelv> nyelvek = new();
+            List<string> sikeresSorok = File.ReadAllLines(sikeres).Skip(1).ToList();
+            List<string> sikertelenSorok = File.ReadAllLines(sikertelen).Skip(1).ToList();
+            for (int i = 0; i < sikeresSorok.Count; i++)
+            {
+                string[] sikeresMezok = sikeresSorok[i].Split(';');
+                string[] sikertelenMezok = sikertelenSorok[i].Split(';');
+
+                nyelvek.Add(new Nyelv(
+                    sikeresMezok[0],
+                    sikeresMezok.Skip(1).Select(x => Convert.ToInt32(x)).ToList(),
+                    sikertelenMezok.Skip(1).Select(x => Convert.ToInt32(x)).ToList()
+                ));
+            }
+            return nyelvek;
+        }
+
+        static void Main(string[] args)
+        {
+            nyelvek = AdatokBetoltese("datas\\sikeres.csv", "datas\\sikertelen.csv");
+
+
+            //2.
+            Console.WriteLine("2. feladat: Legnépszerűbb nyelvek:");
+            nyelvek
+                .OrderByDescending(x => x.EddigiOsszesViszgakSzama)
+                .Take(3)
+                .ToList()
+                .ForEach(x => Console.WriteLine("\t" + x.Nev));
+
+            //3.
+            Console.Write("3. feladat:\n\tVizsgálandó év: ");
+            int ev = Convert.ToInt32(Console.ReadLine());
+            if (ev < 2009 || ev > 2018)
+            {
+                return;
+            }
+
+            //4.
+            Console.WriteLine("4. feladat:");
+
+            //A feladatban nagy probléma az, amikor egy évben egyáltalán nem volt vizsga.
+            //Arra milyen értéket adjon vissza? A rendezés miatt nem mindegy!
+            //Ezeket az eseteket ki kell szűrni!
+            Nyelv? v1 = null, v2 = null, v3 = null, v4 = null, v5 = null;
+
+
+            v1 = nyelvek.Where(x => x.VizsgakSzamaAdottEvben(ev) != 0)
+                        .MinBy(x => x.SikeressegiAranyAdottEvben(ev));  //.NET6-tól !
+
+            v2 = nyelvek.Where(x => x.VizsgakSzamaAdottEvben(ev) != 0)
+                        .MaxBy(x => x.SikertelensegiAranyAdottEvben(ev));  //.NET6-tól !
+
+            v3 = nyelvek.Where(x => x.VizsgakSzamaAdottEvben(ev) != 0)
+                        .OrderBy(x => x.SikeressegiAranyAdottEvben(ev))
+                        .First();
+
+            v4 = nyelvek.OrderBy(x => x.SikeressegiAranyAdottEvben(ev))
+                        .First(x => x.SikeressegiAranyAdottEvben(ev) > 0);
+
+            //Azért nem kell szűrni, mivel amikor nincs vizsga (-1) az érték és a rendezés miatt az előre kerül.
+            v5 = nyelvek.OrderBy(x => x.SikertelensegiAranyAdottEvben(ev))
+                        .Last();
+
+
+            Nyelv legsikertelenebbNyelvAdottEvben = v5;
+
+            Console.WriteLine($"\t{ev}-ben {legsikertelenebbNyelvAdottEvben.Nev} nyelvből a sikertelen vizsgák aránya: {legsikertelenebbNyelvAdottEvben.SikertelensegiAranyAdottEvben(ev):f2}");
+
+
+            if (v1 == v2 && v2 == v3 && v3 == v4 && v4 == v5)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\tMinden megoldási út ugyanazt a megoldást adta! Az eredmények hivatkozási címei megegyeznek.");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+
+            //5.
+            Console.WriteLine("5. feladat:");
+
+            if (nyelvek.Count(x => x.VizsgakSzamaAdottEvben(ev) > 0) == nyelvek.Count)
+            {
+                Console.WriteLine("\tMinden nyelvből volt vizsgázó!");
+            }
+            else
+            {
+                nyelvek.Where(x => x.VizsgakSzamaAdottEvben(ev) == 0)
+                    .ToList()
+                    .ForEach(x => Console.WriteLine("\t" + x.Nev));
+            }
+            //6.
+            File.WriteAllLines("osszesites.csv",
+                nyelvek.Select(x => $"{x.Nev};{x.EddigiOsszesViszgakSzama};" +
+                $"{Math.Round(x.SikeressegiAranyTeljesIdoszakban, 2)}%")
+                );
+        }
+    }
+}
